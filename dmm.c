d40f33e9e6b6733574ff3da55da0d85c36621531
Sujay1844 <sujay1844@protonmail.com>
2023-04-27 15:13:29 +0530

    clean up dmm file

--- dmm.c
+++ dmm.c
@@ -1,76 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a;
-int b = 0;
-long long int l = 1000000;
-int update = 448;
-
-void initt(int *arr) {
-	for (long long int i = 0; i < l; i++) {
-		arr[i] = i+1;
-	}
-}
+long long int length = 1000000;
+int update = 400;
 
 int main() {
-	// printf("a = %d\n", a);
-	// printf("\nfuck you");
-	for (int i = 0; i < 5; i++)
-	{
-		// printf("\n%d", i);
-	}
-	
-	printf("\n\n\n");
 
 	// Memory allocation
 	// malloc
-	int *arr = (int*) malloc(l*sizeof(int));
-	// initt(arr);
-	for (long long int i = 0; i < l; i++) {
+	int *arr = (int*) malloc(length*sizeof(int));
+	for (long long int i = 0; i < length; i++) {
 		arr[i] = i+1;
 	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		// printf("\n%d", arr[i]);
-	}
-
-	// free(arr);
-
 
 	// Clear allocation
 	// calloc is same as malloc but it initializes every value to zero
 
-	int *arr2 = (int*) calloc(l, sizeof(int));
-	// initt(arr2);
-	for (long long int i = 0; i < l; i++) {
+	int *arr2 = (int*) calloc(length, sizeof(int));
+	for (long long int i = 0; i < length; i++) {
 		arr2[i] = i+1;
 	}
 
-	for (int i = 0; i < 10; i++)
-	{
-		// printf("\n%d", arr2[i]);
-	}
-
-	// free(arr2);
-
 	printf("\narr1 = %p", arr);
 	printf("\narr2 = %p", arr2);
+
 	// realloc is used to re-allocate
-	l += update;
-	arr2 = (int*) realloc(arr2, l*sizeof(int));
-	// initt(arr);
+	length += update;
+	arr2 = (int*) realloc(arr2, length*sizeof(int));
 
 	for (int i = 0; i < update; i++)
 	{
-		arr2[l+i-update] = l+i+1-update;
+		arr2[length+i-update] = length+i+1-update;
 	}
 
 	printf("\narr1 = %p", arr);
 	printf("\narr2 = %p", arr2);
-	printf("\narr2 = %ld", arr2[l-1]);
-	printf("\n%ld", arr2[l+48]);
+	printf("\narr2 = %ld", arr2[length-1]);
+	printf("\n%ld", arr2[length+48]);
 
 	free(arr);
 	free(arr2);
+
+	printf("\n\n");
 }
